da20a9345ab8b2ebf59aad87f854a84a29c31dde
owyeh5818713 <owyeh5818713@163.com>
2021-10-15 17:21:28 +0800

    ll

---
+++ src/main/java/com/ssm/pojo/Address.java
@@ -0,0 +1,11 @@
+package com.ssm.pojo;
+
+import lombok.Data;
+
+@Data
+public class Address {
+    private String addressAreaId;
+    private String addressName;
+    private String addressRegionId;
+
+}
