9a5963f16a6152f55ecc4d19cf49997f22bf566d
owyeh5818713 <owyeh5818713@163.com>
2021-10-19 18:00:35 +0800

    10/19

--- src/main/java/com/ssm/mapper/AddressMapper.java
+++ src/main/java/com/ssm/mapper/AddressMapper.java
@@ -1,8 +1,19 @@
 package com.ssm.mapper;
 
+import com.ssm.pojo.Address;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 //地区表接口
 public interface AddressMapper {
+    //绑定注册页面的家庭住址
+    //绑定省份
+    public List<Address> getProvinceAddress();
+    //绑定城市
+    public List<Address> getCityAddress(String addressRegionId);
+    //绑定区
+    public List<Address> getAreaAddress(String addressRegionId);
+
 }
